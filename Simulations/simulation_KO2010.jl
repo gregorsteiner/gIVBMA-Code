7b21f9d7f736e58d39120620b53f113777f8a2c9
gregorsteiner <gregorsteiner7@gmail.com>
2024-10-22 12:50:19 +0100

    Ran simulation

--- Simulations/simulation_KO2010.jl
+++ Simulations/simulation_KO2010.jl
@@ -12,7 +12,7 @@ include("aux_functions.jl")
     In this setup we get a first stage R^2 ≈ 0.1 with c_M = 3/8 and R^2 ≈ 0.01 with c_M = 1/8.
 """
 
-m = 1000
+m = 500
 c_M = [1/8, 3/8]
 
 res50 = map(c -> sim_func(m, 50; type = "KO2010", c_M = c, τ = 0.1), c_M)
