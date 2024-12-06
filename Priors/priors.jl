af8251dbb0ad84980da9958fc30c7042f4b48992
Gregor Steiner <gregorsteiner7@gmail.com>
2024-12-06 08:30:53 +0000

    Cleaned up priors file

--- Priors/priors.jl
+++ Priors/priors.jl
@@ -1,30 +1,6 @@
 
 using StatsPlots, Integrals, SpecialFunctions, Distributions
 
-# Inspect marginal prior of α for different values of ν and g
-plot(Normal(0, 10), xlim = (-20, 20), label = "N(0, 10^2)", xlabel = "α")
-plot!(Normal(0, 5), xlim = (-20, 20), label = "N(0, 5^2)")
-
-function plot_prior(g, ν, n)
-        α = Vector(undef, 1000)
-        σ_y_x = Vector(undef, 1000)
-        for i in eachindex(α)
-                Σ = rand(InverseWishart(ν, [1 0 0; 0 1 0; 0 0 1]))
-                Σ_xx = Σ[2:end, 2:end]
-                Σ_yx = Σ[2:end, 1]
-                σ_y_x[i] = Σ[1,1] - Σ_yx' * inv(Σ_xx) * Σ_yx
-
-                α[i] = rand(Normal(0, σ_y_x[i] * g / n))
-        end
-
-        density!(α, label = "G_prior(g/n = $(g/n), ν = $ν)")
-end
-
-plot_prior(100, 4, 100)
-plot_prior(500, 4, 100)
-plot_prior(100, 3, 100)
-plot_prior(500, 3, 100)
-
 
 # hyperprior on ν
 jp_ν(ν, p) = ((ν+1)/(ν+3))^(p/2) * (ν/(ν+3))^(1/2) * (SpecialFunctions.trigamma(ν/2) -  SpecialFunctions.trigamma((ν+1)/2) - 2*(ν+3)/(ν*(ν+1)^2))^(1/2)
@@ -91,4 +67,3 @@ density!(res_ν3[-10 .< res_ν3 .< 10], label = "fixed ν = 3")
 res_ν5 = map(x -> x[1,2]/x[2, 2], rand(InverseWishart(5, [1 0; 0 1]), 100000))
 density!(res_ν5[-10 .< res_ν5 .< 10], label = "fixed ν = 5")
 savefig(p_exp, "Implied_prior_with_exponential.pdf")
-
