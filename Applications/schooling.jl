e85d024a9e29f8c91bef60b47fc760864fa69380
Gregor Steiner <gregorsteiner7@gmail.com>
2025-01-20 15:43:49 +0000

    Dropped model savinbg

--- Applications/schooling.jl
+++ Applications/schooling.jl
@@ -41,17 +41,6 @@ res_hg_2 = givbma(y_2, X_2, Z_2, W_2; iter = iters, burn = Int(iters/2), g_prior
 res_bric_2 = givbma(y_2, X_2, Z_2, W_2; iter = iters, burn = Int(iters/2), g_prior = "BRIC")
 res_bma_2 = bma(y_2, X_2, W_2; iter = iters, burn = Int(iters/2), g_prior = "hyper-g/n")
 
-# save model objects
-using BSON
-bson("schooling_models.bson", Dict(
-    :HG_A => res_hg_1,
-    :BRIC_A => res_bric_1,
-    :BMA_A => res_bma_1,
-    :HG_B => res_hg_2,
-    :BRIC_B => res_bric_2,
-    :BMA_B => res_bma_2
-))
-
 # Plot with posterior results
 cols = Makie.wong_colors()
 
