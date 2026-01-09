ea62d19540c33dc555de7db5d92db30c02a4dd73
Gregor Steiner <51028286+gregorsteiner@users.noreply.github.com>
2026-01-09 12:34:30 +0100

    Fixed formula and added graph

--- Priors/model_prior.jl
+++ Priors/model_prior.jl
@@ -1,9 +1,9 @@
 
 using SpecialFunctions
-using CairoMakie
+using CairoMakie, LaTeXStrings
 
 # model prior prob. mass function (log-scale)
-function model_prior(x, k, a = 1, m = floor(k/2))
+function model_prior(x, k, a = 1, m = k/2)
     b = (k - m) / m 
     kj = sum(x)
     
@@ -25,29 +25,35 @@ function all_binary_vectors(p::Int)
 end
 
 # prior probability mass function for valid instruments (log-scale)
-function instrument_prior(p)
-    x = all_binary_vectors(p)
-    prob = zeros(p+1)
-    for L in x
-        for M in x
-            n_z = count_zero_one(L, M)
-            prob[n_z + 1] += exp(model_prior(L, p) + model_prior(M, p))
+function instrument_prior(n_z, p)
+    total = 0.0
+    Γ(x) = SpecialFunctions.gamma(x)
+    for p_i in 0:p
+        for k in 0:p_i
+            if n_z <= (p - p_i)
+                total += binomial(p_i, p_i - k) * binomial(p - p_i, n_z) * Γ(1 + p_i + n_z - k) * Γ(1 + p - p_i - n_z + k) / Γ(p + 2)
+            end
         end
     end
-    return prob
+    return total / (p+1)
 end
 
 
 # Illustrate in plot
-ps = [2, 5, 10, 15]
-fig = Figure(size=(1200, 800))
+ps = [5, 10, 15, 20]
+fig = Figure(size = (1000, 800))
 
 for (i, p) in enumerate(ps)
-    prob = instrument_prior(p)
-    ax = Axis(fig[1, i], xlabel="Number of Valid Instruments", ylabel="Prior Probability", 
-              title="p = $p")
-    barplot!(ax, 0:p, prob, color=:steelblue4, width=0.8)
+    row = (i - 1) ÷ 2 + 1  # 1,1,2,2
+    col = (i - 1) % 2 + 1  # 1,2,1,2
+    prob = [instrument_prior(n_z, p) for n_z in 0:p]
+    ax = Axis(fig[row, col], xlabel = "Number of valid instruments", 
+              ylabel = "Prior Probability", title = "p = $p",
+              xgridvisible = false, ygridvisible = false)
+    #hidespines!(ax, :t, :r)
+    barplot!(ax, 0:p, prob, alpha = 0.7, color = :steelblue4)
 end
 
 display(fig)
 
+save("Implied_instrument_prior.pdf", fig)
