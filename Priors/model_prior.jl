dbf66a3b1b3b44c2a5c5614ca637ca33ff7c967f
Gregor Steiner <51028286+gregorsteiner@users.noreply.github.com>
2026-01-12 13:14:58 +0100

    Added function to extract the number of instruments

--- Priors/model_prior.jl
+++ Priors/model_prior.jl
@@ -2,6 +2,8 @@
 using SpecialFunctions
 using CairoMakie, LaTeXStrings
 
+include("../Simulations/aux_functions.jl")
+
 # model prior prob. mass function (log-scale)
 function model_prior(x, k, a = 1, m = k/2)
     b = (k - m) / m 
@@ -12,17 +14,6 @@ function model_prior(x, k, a = 1, m = k/2)
     return res
 end
 
-
-# function that counts the number of valid instruments in a model
-function count_zero_one(a, b)
-    length(a) == length(b) || throw(ArgumentError("Vectors must have the same length"))
-    return sum((a .== 0) .& (b .== 1))
-end
-
-# helper function that creates all possible models
-function all_binary_vectors(p::Int)
-    [digits(i, base = 2, pad=p) for i in 0:2^p-1]
-end
 
 # prior probability mass function for valid instruments (log-scale)
 function instrument_prior(n_z, p)
